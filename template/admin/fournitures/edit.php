17b8b02973dd27a5dc9d502846e2188c024a7684
apprenti1 <elias.aitboukha@gmail.com>
2024-06-11 23:24:27 +0200

    upd

--- template/admin/fournitures/edit.php
+++ template/admin/fournitures/edit.php
@@ -5,7 +5,7 @@
     <div></div>
     <input type="text" name="titre" placeholder="Description" value="<?= (isset($_POST['edit']))? $entity->getTitre() : "" ?>">
     <div class="d-flex">
-        <input type="number" step="0,01" name="prix" placeholder="prix" value="<?= (isset($_POST['edit']))? $entity->getPrix() : "" ?>">
+        <input type="number" step="0.01" name="prix" placeholder="prix" value="<?= (isset($_POST['edit']))? $entity->getPrix() : "" ?>">
         <p>€</p>
     </div>
     <input type="submit" name="return" value="<?= (isset($_POST['edit']))? "edit" : "new" ?>">
