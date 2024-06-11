75b4b421c9599a1284f1d710e2f20bd527d94a26
apprenti1 <elias.aitboukha@gmail.com>
2024-06-11 22:47:12 +0200

    upd

--- template/admin/index.php
+++ template/admin/index.php
@@ -1,21 +1,21 @@
 <div class="container d-flex justify-content-center align-items-center h-100">
     <div class="flex-fill d-flex justify-content-center align-items-center flex-column p-2 m-1 ">
-        <a href="../admin/devis">
+        <a href="<?= $baseurl ?>admin/devis">
             Devis
         </a>
     </div>
     <div class="flex-fill d-flex justify-content-center align-items-center flex-column p-2 m-1 ">
-        <a href="../admin/produits">
+        <a href="<?= $baseurl ?>admin/produits">
             Produits
         </a>
     </div>
     <div class="flex-fill d-flex justify-content-center align-items-center flex-column p-2 m-1 ">
-        <a href="../admin/fournitures">
+        <a href="<?= $baseurl ?>admin/fournitures">
             Fournitures
         </a>
     </div>
     <div class="flex-fill d-flex justify-content-center align-items-center flex-column p-2 m-1 ">
-        <a href="../admin/disconnect">
+        <a href="<?= $baseurl ?>admin/disconnect">
             Se déconnecter
         </a>
     </div>
